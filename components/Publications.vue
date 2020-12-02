3904c0813229feea31e22047eeb2f1083f2a6fd4
gatheluck <gatheluck+tech@gmail.com>
2020-12-03 07:37:11 +0900

    adjust publications

--- components/Publications.vue
+++ components/Publications.vue
@@ -1,9 +1,13 @@
 <template>
-  <div>
+  <div class="pub">
+    <h2>Publications</h2>
+    <hr />
+
+    <h3>Preprint</h3>
     <b-card
       title="ATRO: Adversarial Training with a Rejection Option"
       sub-title="Masahiro Kato, Zhenghang Cui, Yoshihiro Fukuhara"
-      class="border-white mb-n3 pub"
+      class="border-0 bg-transparent mt-n3"
     >
       <b-card-text>
         arXiv preprint.
@@ -12,11 +16,31 @@
       </b-card-text>
     </b-card>
 
+    <h3>Journal</h3>
+    <b-card
+      title="Property Analysis of Adversarially Robust Representation"
+      sub-title="Yoshihiro Fukuhara, Takahiro Itazuri, Hirokatsu Kataoka, Shigeo Morishima"
+      class="border-0 bg-transparent mt-n3"
+    >
+      <b-card-text>
+        Journal of Japan Society for Precision Engineering (JSPE), Vol.87, No.1,
+        pp., 2021.01.
+        <br />
+        <a href="https://arxiv.org/abs/1811.06943" target="_blank">[arXiv]</a>
+        <a
+          href="https://cvpaperchallenge.github.io/AutoPaperSummaryGen/"
+          target="_blank"
+          >[Project]</a
+        >
+      </b-card-text>
+    </b-card>
+
+    <h3>International Conference</h3>
     <!-- Paper Summary Gen -->
     <b-card
       title="Automatic Paper Summary Generation from Visual and Textual Information"
       sub-title="Shintaro Yamamoto*, Yoshihiro Fukuhara*, Ryota Suzuki, Shigeo Morishima, Hirokatsu Kataoka"
-      class="border-white mb-n3 pub"
+      class="border-0 bg-transparent mt-n3"
     >
       <b-card-text>
         International Conference on Machine Vision (ICMV), 2018. (* indicates
@@ -35,7 +59,7 @@
     <b-card
       title="Understanding Fake Faces"
       sub-title="Ryota Natsume*, Kazuki Inoue*, Yoshihiro Fukuhara, Shintaro Yamamoto, Shigeo Morishima, Hirokatsu Kataoka"
-      class="border-white mb-n3 pub"
+      class="border-0 bg-transparent mt-n3"
     >
       <b-card-text>
         ECCV 2018 Workshop on Brain-Driven Computer Vision (BDCV). (* indicates
@@ -49,7 +73,7 @@
     <b-card
       title="Fiber-Dependent Approach for Fast Dynamic Character Animation"
       sub-title="Ayano Kaneda,Tsukasa Fukusato,Yoshihiro Fukuhara,Takayuki Nakatsuka, Shigeo Morishima"
-      class="border-white mb-n3 pub"
+      class="border-0 bg-transparent mt-n3"
     >
       <b-card-text>
         Conference on Computer Animation and Social Agents (CASA), 2017.
@@ -65,6 +89,14 @@
 </template>
 
 <style>
+.pub h2 {
+  font-size: 24px;
+}
+
+.pub h3 {
+  font-size: 20px;
+}
+
 .pub h4 {
   font-size: 18px;
 }
