9a851da0e29153123c755112f6da1dd9d50ea76a
gatheluck <gatheluck+tech@gmail.com>
2021-01-16 10:00:39 +0900

    feat: conbine grid system and card to scale image size.

--- components/Top.vue
+++ components/Top.vue
@@ -1,35 +1,47 @@
 <template>
   <div class="top mt-5">
-    <b-card
-      title="Yoshihiro Fukuhara"
-      title-tag="h1"
-      img-src="../static/image/icon.png"
-      img-height="200"
-      img-left
-      class="mb-3 border-0"
-    >
-      <b-card-text>
-        <p>
-          I am currently a third-year PhD student at Waseda University, where I
-          am advised by Professor
-          <a
-            href="https://scholar.google.co.jp/citations?user=4B-C50EAAAAJ&hl=en"
-            target="_blank"
-            >Shigeo Morishima</a
-          >. I am also a member of
-          <a href="http://www.leading-sn.waseda.ac.jp/"
-            >Waseda Innovation Leading Laboratory (WILL)</a
+    <b-card no-body class="border-0">
+      <b-row no-gutters>
+        <b-col class="col-2">
+          <b-card-img
+            src="../static/image/icon.png"
+            alt="Image"
+            class="rounded-circle"
+          ></b-card-img>
+        </b-col>
+        <b-col class="mt-n3">
+          <b-card-body
+            title="Yoshihiro Fukuhara"
+            title-tag="h1"
+            sub-title="aka gatheluck"
+            sub-title-tag="h2"
           >
-          and <a href="http://xpaperchallenge.org/cv/">cvpaper.challenge</a>. My
-          research interests are primarily in Robust Machine Learning to develop
-          ML tools that are robust, reliable and ready for real-world
-          deployment.
-        </p>
-        <p>
-          I prefer to study new things and always try to keep changing. Nothing
-          is permanent.
-        </p>
-      </b-card-text>
+            <b-card-text>
+              <p>
+                I am currently a third-year PhD student at Waseda University,
+                where I am advised by Professor
+                <a
+                  href="https://scholar.google.co.jp/citations?user=4B-C50EAAAAJ&hl=en"
+                  target="_blank"
+                  >Shigeo Morishima</a
+                >. I am also a member of
+                <a href="http://www.leading-sn.waseda.ac.jp/"
+                  >Waseda Innovation Leading Laboratory (WILL)</a
+                >
+                and
+                <a href="http://xpaperchallenge.org/cv/">cvpaper.challenge</a>.
+                My research interests are primarily in Robust Machine Learning
+                to develop ML tools that are robust, reliable and ready for
+                real-world deployment.
+              </p>
+              <p>
+                I prefer to study new things and always try to keep changing.
+                Nothing is permanent.
+              </p>
+            </b-card-text>
+          </b-card-body>
+        </b-col>
+      </b-row>
     </b-card>
   </div>
 </template>
