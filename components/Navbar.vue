15f3c4d8aa35966007f4551f4ca659602b28f99a
gatheluck <gatheluck+tech@gmail.com>
2021-01-16 10:53:23 +0900

    refactor: add container inside of navbar.

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,55 +1,60 @@
 <template>
-  <!-- "variant" means  -->
   <b-navbar toggleable="md" type="dark" sticky="true" variant="secondary">
-    <b-navbar-brand href="#">
-      <b-img
-        rounded="circle"
-        height="25"
-        src="../static/image/robust_feature_cropped.png"
-        class="mr-1"
-      />
-      Yoshihiro Fukuhara
-    </b-navbar-brand>
+    <b-container>
+      <b-navbar-brand href="#">
+        <b-img
+          rounded="circle"
+          height="25"
+          src="../static/image/robust_feature_cropped.png"
+          class="mr-1"
+        />
+        Yoshihiro Fukuhara
+      </b-navbar-brand>
 
-    <ul class="navbar-nav flex-row ml-md-auto">
-      <li class="nav-item">
-        <a
-          class="unstyled nav-link"
-          href="https://github.com/gatheluck"
-          target="_blank"
-        >
-          <font-awesome-icon class="mr-2" :icon="['fab', 'github']" size="lg" />
-        </a>
-      </li>
+      <ul class="navbar-nav flex-row ml-md-auto">
+        <li class="nav-item">
+          <a
+            class="unstyled nav-link"
+            href="https://github.com/gatheluck"
+            target="_blank"
+          >
+            <font-awesome-icon
+              class="mr-2"
+              :icon="['fab', 'github']"
+              size="lg"
+            />
+          </a>
+        </li>
 
-      <li class="nav-item">
-        <a
-          class="unstyled nav-link"
-          href="https://twitter.com/gatheluck"
-          target="_blank"
-        >
-          <font-awesome-icon
-            class="mr-2"
-            :icon="['fab', 'twitter']"
-            size="lg"
-          />
-        </a>
-      </li>
+        <li class="nav-item">
+          <a
+            class="unstyled nav-link"
+            href="https://twitter.com/gatheluck"
+            target="_blank"
+          >
+            <font-awesome-icon
+              class="mr-2"
+              :icon="['fab', 'twitter']"
+              size="lg"
+            />
+          </a>
+        </li>
 
-      <li class="nav-item">
-        <a
-          class="unstyled nav-link"
-          href="https://www.linkedin.com/in/yoshihiro-fukuhara-01b329136/"
-          target="_blank"
-        >
-          <font-awesome-icon
-            class="mr-2"
-            :icon="['fab', 'linkedin-in']"
-            size="lg"
-          />
-        </a>
-      </li>
-    </ul>
+        <li class="nav-item">
+          <a
+            class="unstyled nav-link"
+            href="https://www.linkedin.com/in/yoshihiro-fukuhara-01b329136/"
+            target="_blank"
+          >
+            <font-awesome-icon
+              class="mr-2"
+              :icon="['fab', 'linkedin-in']"
+              size="lg"
+            />
+          </a>
+        </li>
+      </ul>
+    </b-container>
   </b-navbar>
 </template>
 
