a87064b702b56888ce3ad3cd8166b2ee8bc30d6e
gatheluck <gatheluck+tech@gmail.com>
2021-01-20 14:14:17 +0900

    feat: use v-for to loop over data.

--- components/Biography.vue
+++ components/Biography.vue
@@ -1,141 +1,70 @@
 <template>
-  <div class="me">
+  <div class="bio">
     <h2>About Me</h2>
     <hr />
 
-    <div class="bio">
-      <h3>Biography</h3>
-      <!-- myProduct -->
-      <b-card
-        title="2020.06 - ongoing: myProduct Inc. AI Engineer."
-        title-tag="h6"
-        class="border-0 bg-transparent mt-n3"
-      >
-        <b-card-text class="mt-n2 mb-n3"> </b-card-text>
-      </b-card>
+    <h3>Biography</h3>
+    <div class="mb-3"></div>
+    <b-card
+      v-for="bio in aboutme.biography"
+      :key="bio.title"
+      no-body
+      align="left"
+      class="border-0"
+    >
+      <b-row no-gutters>
+        <!-- left side -->
+        <b-col xl="2" lg="3" md="4">
+          <div class="bio-date">
+            <b-card-text class="pl-3">{{ bio.date }}:</b-card-text>
+          </div>
+        </b-col>
+        <!-- right side -->
+        <b-col xl="10" lg="9" md="8" class="mt-n3 mb-n2">
+          <div class="bio-text">
+            <b-card-body>
+              <!-- title -->
+              <b-card-title title-tag="h4">
+                <span v-html="bio.title"></span>
+              </b-card-title>
 
-      <!-- AIST -->
-      <b-card
-        title="2020.04 - ongoing: The National Institute of Advanced Industrial Science and Technology (AIST). Research Assistant."
-        title-tag="h6"
-        class="border-0 bg-transparent mt-n4"
-      >
-        <b-card-text class="mt-n2 mb-n2">
-          Supervisor:
-          <a href="http://hirokatsukataoka.net/" target="_blank">
-            Hirokatsu, Kataoka, Ph.D.
-          </a>
-        </b-card-text>
-      </b-card>
-
-      <!-- Doctor -->
-      <b-card
-        title="2018.04 - ongoing: Department of Applied Physics, Waseda University, Japan."
-        title-tag="h6"
-        class="border-0 bg-transparent mt-n4"
-      >
-        <b-card-text class="mt-n2 mb-n2">
-          <a href="http://www.leading-sn.waseda.ac.jp/" target="_blank">
-            Waseda University Leading Doctoral Course Embodiment Informatics
-          </a>
-        </b-card-text>
-      </b-card>
-
-      <!-- SE4 -->
-      <b-card
-        title="2018.10 - 2020.03: SE4 Inc. Research Internship."
-        title-tag="h6"
-        class="border-0 bg-transparent mt-n4"
-      >
-        <b-card-text class="mt-n2 mb-n3"> </b-card-text>
-      </b-card>
-
-      <!-- FOVE -->
-      <b-card
-        title="2018.02 - 2018.09: Fove Inc. Research Internship."
-        title-tag="h6"
-        class="border-0 bg-transparent mt-n4"
-      >
-        <b-card-text class="mt-n2 mb-n3"> </b-card-text>
-      </b-card>
-
-      <!-- Master -->
-      <b-card
-        title="2016.04 - 2018.03: Department of Applied Physics, Waseda University, Japan."
-        title-tag="h6"
-        class="border-0 bg-transparent mt-n4"
-      >
-        <b-card-text class="mt-n2 mb-n2"> Master of Engineering </b-card-text>
-      </b-card>
-
-      <!-- RA -->
-      <b-card
-        title="2014.06 - 2018.03: Waseda University, Japan. Resident Assistant (RA)."
-        title-tag="h6"
-        class="border-0 bg-transparent mt-n4"
-      >
-        <b-card-text class="mt-n2 mb-n2">
-          Waseda International Dormitory (WID) Kodaira
-          <br />
-          <a href="https://www.waseda.jp/inst/rlc/assistant/" target="_blank">
-            [About RA]
-          </a>
-        </b-card-text>
-      </b-card>
-
-      <!-- CSST -->
-      <b-card
-        title="2016.07 - 2016.09: University of California, Los Angeles. Visiting Student."
-        title-tag="h6"
-        class="border-0 bg-transparent mt-n4"
-      >
-        <b-card-text class="mt-n2 mb-n2">
-          <a href="https://www.csst.ucla.edu/" target="_blank">
-            Cross-disciplinary Scholars in Science and Technology (CSST)
-          </a>
-          2016 Participants
-          <br />
-          <a href="https://www.csst.ucla.edu/" target="_blank">
-            [Alumni List]
-          </a>
-          (Acceptance rate of Japanese Students:0.1%）
-          <br />
-          Supervisor:
-          <a href="http://web.cs.ucla.edu/~soatto/" target="_blank">
-            Prof. Stefano Soatto
-          </a>
-        </b-card-text>
-      </b-card>
-
-      <!-- Bachelor -->
-      <b-card
-        title="2012.04 - 2016.03: Department of Applied Physics, Waseda University, Japan."
-        title-tag="h6"
-        class="border-0 bg-transparent mt-n4"
-      >
-        <b-card-text class="mt-n2"> Bachelor of Engineering </b-card-text>
-      </b-card>
-    </div>
-    <!-- end of bio -->
+              <!-- texts -->
+              <div class="mt-n2"></div>
+              <b-card-text v-for="text in bio.texts" :key="text" class="mb-0">
+                <span v-html="text"></span>
+              </b-card-text>
+            </b-card-body>
+          </div>
+        </b-col>
+      </b-row>
+    </b-card>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    const aboutme = require('~/assets/aboutme.json')
+    return {
+      aboutme,
+    }
+  },
+}
+</script>
+
 <style>
-h2 {
-  font-size: 24px;
-}
-
-.bio h3 {
-  font-size: 20px;
-}
-
-.bio h6 {
+.bio h4 {
   font-size: 16px;
 }
 
-.bio p {
-  color: #6c757d;
+.bio-date p {
+  font-size: 16px;
+}
+
+.bio-text p {
   font-size: 14px;
-  margin-left: 8.9rem;
+  color: #6c757d;
+  margin-bottom: 0;
+  margin-top: 0;
 }
 </style>
