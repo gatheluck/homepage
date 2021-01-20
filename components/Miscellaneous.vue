30e62102e8425c0e84294f44f5fbb345f3435b15
gatheluck <gatheluck+tech@gmail.com>
2021-01-20 21:42:47 +0900

    feat: use v-for to loop over data.

--- components/Miscellaneous.vue
+++ components/Miscellaneous.vue
@@ -3,215 +3,264 @@
     <h2>Miscellaneous</h2>
     <hr />
 
-    <div class="awards">
+    <div class="award">
       <h3>Award</h3>
-      <!-- YANS -->
+      <div class="mb-3"></div>
       <b-card
-        title="2018.08: Incentive Award, Symposium on Young Research Association for NLP Studies (YANS), 2018."
-        title-tag="h6"
-        class="border-0 bg-transparent mt-n3"
+        v-for="award in miscellaneous.award"
+        :key="award.title"
+        no-body
+        align="left"
+        class="border-0"
       >
-        <b-card-text class="mt-n2 mb-n3"> </b-card-text>
-      </b-card>
+        <b-row no-gutters>
+          <!-- left side -->
+          <b-col xl="1" lg="1" md="2">
+            <div class="award-date">
+              <b-card-text class="pl-3">{{ award.date }}</b-card-text>
+            </div>
+          </b-col>
+          <!-- right side -->
+          <b-col xl="11" lg="11" md="10" class="mt-n3 mb-n2">
+            <div class="award-text">
+              <b-card-body>
+                <!-- title -->
+                <b-card-title title-tag="h4">
+                  <span v-html="award.title"></span>
+                </b-card-title>
 
-      <!-- Mars -->
-      <b-card
-        title="2017.12: JAXA Special Award, HP Mars Home Planet - Education League JP-."
-        title-tag="h6"
-        class="border-0 bg-transparent mt-n4"
-      >
-        <b-card-text class="mt-n2 mb-n3"> </b-card-text>
-      </b-card>
-
-      <!-- VIP -->
-      <b-card
-        title="2015.09: Performance Award, Visual Infromation Processing Camp (VIP), 2015."
-        title-tag="h6"
-        class="border-0 bg-transparent mt-n4"
-      >
-        <b-card-text class="mt-n2 mb-n3"> </b-card-text>
+                <!-- texts -->
+                <div class="mt-n2"></div>
+                <b-card-text
+                  v-for="text in award.texts"
+                  :key="text"
+                  class="mb-0"
+                >
+                  <span v-html="text"></span>
+                </b-card-text>
+              </b-card-body>
+            </div>
+          </b-col>
+        </b-row>
       </b-card>
     </div>
     <!-- end of awards -->
 
-    <div class="talks">
+    <div class="talk">
       <h3>Talk</h3>
-      <!-- SSII 2020 -->
+      <div class="mb-3"></div>
       <b-card
-        title="2020.06: Adversarial Examples 研究動向"
-        title-tag="h6"
-        class="border-0 bg-transparent mt-n3"
+        v-for="talk in miscellaneous.talk"
+        :key="talk.title"
+        no-body
+        align="left"
+        class="border-0"
       >
-        <b-card-text class="mt-n2 mb-n2">
-          Invited Talk, Symposium of Sensing via Image Infomation (SSII), 2020.
-          <br />
-          <a
-            href="https://confit.atlas.jp/guide/event/ssii2020/static/invited_interactive#IS2"
-            target="_blank"
-          >
-            [SSII 2020]
-          </a>
-        </b-card-text>
-      </b-card>
+        <b-row no-gutters>
+          <!-- left side -->
+          <b-col xl="1" lg="1" md="2">
+            <div class="talk-date">
+              <b-card-text class="pl-3">{{ talk.date }}</b-card-text>
+            </div>
+          </b-col>
+          <!-- right side -->
+          <b-col xl="11" lg="11" md="10" class="mt-n3 mb-n2">
+            <div class="talk-text">
+              <b-card-body>
+                <!-- title -->
+                <b-card-title title-tag="h4">
+                  <span v-html="talk.title"></span>
+                </b-card-title>
 
-      <!-- CVPR 2019 -->
-      <b-card
-        title="2019.09: Adversarial Examples 分野の動向 -History and Trends of Dark Arts-"
-        title-tag="h6"
-        class="border-0 bg-transparent mt-n4"
-      >
-        <b-card-text class="mt-n2 mb-n2">
-          cvpaper.challenge CVPR 2019 網羅的サーベイ報告会 @日立製作所
-          中央研究所.
-          <br />
-          <a
-            href="https://www.slideshare.net/cvpaperchallenge/adversarial-examples-173590674"
-            target="_blank"
-          >
-            [Slide]
-          </a>
-          <a
-            href="https://www.slideshare.net/cvpaperchallenge/adversarial-examples-229232499"
-            target="_blank"
-          >
-            [Slide(+Meta Survey)]
-          </a>
-        </b-card-text>
+                <!-- texts -->
+                <div class="mt-n2"></div>
+                <b-card-text
+                  v-for="text in talk.texts"
+                  :key="text"
+                  class="mb-0"
+                >
+                  <span v-html="text"></span>
+                </b-card-text>
+              </b-card-body>
+            </div>
+          </b-col>
+        </b-row>
       </b-card>
     </div>
     <!-- end of talks -->
 
     <div class="funding">
       <h3>Funding</h3>
-      <!-- Leading -->
+      <div class="mb-3"></div>
       <b-card
-        title="2016.04 - 2020.03: Graduate Program for Embodiment Informatics Scholarship."
-        title-tag="h6"
-        class="border-0 bg-transparent mt-n3"
+        v-for="funding in miscellaneous.funding"
+        :key="funding.title"
+        no-body
+        align="left"
+        class="border-0"
       >
-        <b-card-text class="mt-n2 mb-n3"> </b-card-text>
-      </b-card>
+        <b-row no-gutters>
+          <!-- left side -->
+          <b-col xl="2" lg="2" md="3">
+            <div class="funding-date">
+              <b-card-text class="pl-3">{{ funding.date }}</b-card-text>
+            </div>
+          </b-col>
+          <!-- right side -->
+          <b-col xl="10" lg="10" md="9" class="mt-n3 mb-n2">
+            <div class="funding-text">
+              <b-card-body>
+                <!-- title -->
+                <b-card-title title-tag="h4">
+                  <span v-html="funding.title"></span>
+                </b-card-title>
 
-      <!-- CSST 2016 -->
-      <b-card
-        title="2016.07 - 2016.09: University of California, Los Angeles, Cross-disciplinary Scholars in Science and Technology (CSST) Scholarship Program 2016."
-        title-tag="h6"
-        class="border-0 bg-transparent mt-n4"
-      >
-        <b-card-text class="mt-n2 mb-n3"> </b-card-text>
-      </b-card>
-
-      <!-- CSST 2018 -->
-      <b-card
-        title="2012.04 - 2013.03: Waseda University, Okuma Memorial Special Scholarship."
-        title-tag="h6"
-        class="border-0 bg-transparent mt-n4"
-      >
-        <b-card-text class="mt-n2 mb-n3"> </b-card-text>
+                <!-- texts -->
+                <div class="mt-n2"></div>
+                <b-card-text
+                  v-for="text in funding.texts"
+                  :key="text"
+                  class="mb-0"
+                >
+                  <span v-html="text"></span>
+                </b-card-text>
+              </b-card-body>
+            </div>
+          </b-col>
+        </b-row>
       </b-card>
     </div>
     <!-- end of Funding -->
 
     <div class="patent">
       <h3>Patent</h3>
-      <!-- Leading -->
       <b-card
-        title="画像処理装置及び画像処理方法"
-        title-tag="h6"
-        class="border-0 bg-transparent mt-n3"
+        v-for="patent in miscellaneous.patent"
+        :key="patent.title"
+        no-body
+        align="left"
+        class="border-0"
       >
-        <b-card-text class="mt-n2 mb-n2">
-          特開 2018-195038 (P2018-195038A)
-          <br />
-          <a href="https://ipforce.jp/patent-jp-A-2018-195038" target="_blank">
-            [IP Force]
-          </a>
-        </b-card-text>
+        <b-card-body class="mt-n2 mb-n1">
+          <!-- title -->
+          <b-card-title title-tag="h4">
+            <span v-html="patent.title"></span>
+          </b-card-title>
+
+          <!-- texts -->
+          <div class="mt-n1"></div>
+          <b-card-text v-for="text in patent.texts" :key="text" class="mb-0">
+            <span v-html="text"></span>
+          </b-card-text>
+        </b-card-body>
       </b-card>
     </div>
     <!-- end of Patent -->
 
-    <div class="certifications">
+    <div class="certification">
       <h3>Certification</h3>
-      <!-- Leading -->
       <b-card
-        title="CG-ARTS CG Engineer Examination - Expert"
-        title-tag="h6"
-        class="border-0 bg-transparent mt-n3"
+        v-for="cert in miscellaneous.certification"
+        :key="cert.title"
+        no-body
+        align="left"
+        class="border-0"
       >
+        <b-card-body class="mt-n2 mb-n1">
+          <!-- title -->
+          <b-card-title title-tag="h4">
+            <span v-html="cert.title"></span>
+          </b-card-title>
+
+          <!-- texts -->
+          <div class="mt-n1"></div>
+          <b-card-text v-for="text in cert.texts" :key="text" class="mb-0">
+            <span v-html="text"></span>
+          </b-card-text>
+        </b-card-body>
       </b-card>
     </div>
     <!-- end of Certifications -->
   </div>
+  <!-- end of Miscellaneous -->
 </template>
 
+<script>
+export default {
+  data() {
+    const miscellaneous = require('~/assets/miscellaneous.json')
+    return {
+      miscellaneous,
+    }
+  },
+}
+</script>
+
 <style>
-h2 {
-  font-size: 24px;
-}
-
-.awards h3 {
-  font-size: 20px;
-}
-
-.awards h6 {
+.award h4 {
   font-size: 16px;
 }
 
-.awards p {
+.award-date p {
+  font-size: 16px;
+}
+
+.award-text p {
   font-size: 14px;
-  margin-left: 4.5rem;
-}
-
-.funding h3 {
-  font-size: 20px;
-}
-
-.funding h6 {
-  font-size: 16px;
-}
-
-.funding p {
-  font-size: 14px;
-  margin-left: 4.3rem;
-}
-
-.talks h3 {
-  font-size: 20px;
-}
-
-.talks h6 {
-  font-size: 16px;
-}
-
-.talks p {
   color: #6c757d;
+  margin-bottom: 0;
+  margin-top: 0;
+}
+
+.talk h4 {
+  font-size: 16px;
+}
+
+.talk-date p {
+  font-size: 16px;
+}
+
+.talk-text p {
   font-size: 14px;
-  margin-left: 4.3rem;
+  color: #6c757d;
+  margin-bottom: 0;
+  margin-top: 0;
 }
 
-.patent h3 {
-  font-size: 20px;
+.funding h4 {
+  font-size: 16px;
 }
 
-.patent h6 {
+.funding-date p {
+  font-size: 16px;
+}
+
+.funding-text p {
+  font-size: 14px;
+  color: #6c757d;
+  margin-bottom: 0;
+  margin-top: 0;
+}
+
+.patent h4 {
   font-size: 16px;
 }
 
 .patent p {
-  color: #6c757d;
   font-size: 14px;
+  color: #6c757d;
+  margin-bottom: 0;
+  margin-top: 0;
 }
 
-.certifications h3 {
-  font-size: 20px;
-}
-
-.certifications h6 {
+.certification h4 {
   font-size: 16px;
 }
 
-.certifications p {
+.certification p {
   font-size: 14px;
+  color: #6c757d;
+  margin-bottom: 0;
+  margin-top: 0;
 }
 </style>
