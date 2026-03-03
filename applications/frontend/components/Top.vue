8ba6a3fda33600980f00d9099828517d76e1e0b2
gatheluck <gatheluck@gmail.com>
2026-03-03 23:57:48 +0900

    feat: update bio.

--- applications/frontend/components/Top.vue
+++ applications/frontend/components/Top.vue
@@ -18,26 +18,24 @@
           >
             <b-card-text>
               <p>
-                I am a Staff ML/MLOps Engineer at
-                <a href="https://exawizards.com" target="_blank"
-                  >ExaWizards Inc.</a
-                >, where, as a 12×AWS-Certified engineer, I spearhead the Cloud
-                Enabling Guild, driving the seamless transition from
-                proof-of-concept prototypes to robust, scalable machine learning
-                services. In my role within the CTO office, I actively shape our
-                overarching technology strategy to ensure that innovation aligns
-                with our business objectives. With a dual background as both a
-                researcher and an engineer, I am dedicated to rapidly
-                transforming cutting-edge research into practical solutions that
-                deliver meaningful, real-world impact.
+                I am a Senior Research Engineer at
+                <a href="https://caddi.com/" target="_blank">CADDi</a>, where I
+                lead the AI research team. In this role, I work on developing
+                and validating AI technologies that unleash the potential of the
+                manufacturing industry, while shaping the strategy for
+                foundational AI capabilities that will build the company’s
+                long-term moat. With a dual background as both a researcher and
+                an engineer, I am dedicated to rapidly transforming cutting-edge
+                research into practical solutions that deliver meaningful,
+                real-world impact.
               </p>
               <p>
                 Beyond industry, I serve as a core member of
                 <a href="http://xpaperchallenge.org/cv" target="_blank"
                   >cvpaper.challenge</a
                 >, coordinating global collaborations and industry-academia
-                initiatives. In this context I also conduct research as an
-                Adjunct Researcher at
+                initiatives. In this context I also conduct research as a
+                Specially Appointed Fellow at
                 <a href="https://www.aist.go.jp/index_en.html" target="_blank"
                   >AIST</a
                 >
